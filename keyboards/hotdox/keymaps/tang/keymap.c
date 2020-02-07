d42435d801d46ba5d51b3d02203f8e85dcac1495
tang <eytang8@gmail.com>
2020-02-06 20:42:36 -0800

    modifying gui keys for mac

--- keyboards/hotdox/keymaps/tang/keymap.c
+++ keyboards/hotdox/keymaps/tang/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  A(KC_F1),
         LT(_MDIA, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,
         KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,  A(KC_SCLN),
-        KC_LCTL,        KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
+        KC_LCTL,        KC_LSFT, KC_LGUI, KC_LALT, KC_LGUI,
 
                                                        ALT_T(KC_APP), KC_LGUI,
                                                                       KC_HOME,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_F23,     KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,  KC_BSPC,
                          KC_H,   KC_J,       KC_K,    KC_L, KC_SCLN,  KC_QUOT,
              KC_F24,     KC_N,   KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                                  KC_RALT, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,
+                                  KC_RGUI, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,
 
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
