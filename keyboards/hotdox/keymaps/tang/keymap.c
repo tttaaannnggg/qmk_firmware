7c13a5d8258251e687aee87df82a2a4986f36e90
tang <eytang8@gmail.com>
2020-02-06 20:40:39 -0800

    adding my keymap

--- keyboards/hotdox/keymaps/tang/keymap.c
+++ keyboards/hotdox/keymaps/tang/keymap.c
@@ -40,19 +40,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
         KC_GRV,            KC_1,    KC_2,    KC_3,    KC_4,   KC_5,  TG(_GAME),
-        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  MO(_SYM),
+        KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,  A(KC_F1),
         LT(_MDIA, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,
-        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,  MO(_NUM),
+        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,  A(KC_SCLN),
         KC_LCTL,        KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL,
 
                                                        ALT_T(KC_APP), KC_LGUI,
                                                                       KC_HOME,
-                                LT(_NUM,KC_SPC), MT(MOD_LGUI,KC_TAB), KC_LGUI,
+                                LT(_NUM,KC_SPC), MT(MOD_LALT,KC_TAB), KC_LGUI,
         // right hand
              KC_RGHT,     KC_6,   KC_7,       KC_8,    KC_9,    KC_0,  KC_MINS,
-             TG(_SYM),    KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,  KC_BSPC,
-                          KC_H,   KC_J,       KC_K,    KC_L, KC_SCLN,  KC_QUOT,
-             MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+             KC_F23,     KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,  KC_BSPC,
+                         KC_H,   KC_J,       KC_K,    KC_L, KC_SCLN,  KC_QUOT,
+             KC_F24,     KC_N,   KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                                   KC_RALT, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,
 
              KC_LALT,        CTL_T(KC_ESC),
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                KC_TRNS,KC_TRNS,KC_TRNS,
        // right hand
        KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_MINS, KC_MINS,
+       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_0, KC_MINS,
                 KC_DOWN, KC_4,   KC_5,    KC_6,    KC_EQL, KC_F12,
        KC_TRNS, KC_AMPR, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_TRNS,
                          KC_0, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
@@ -128,9 +128,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SYM] = LAYOUT_ergodox(  // layer 2 : symmbols
         // left hand
         KC_GRV,            KC_F1,         KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_LEFT,
-        KC_Q,            KC_SLSH,       KC_LCBR, KC_LABK, KC_LBRC, KC_LPRN, TG(_SYM),
+        KC_Q,            KC_SLSH,       KC_LCBR, KC_LABK, KC_LBRC, KC_LPRN, KC_F21,
         KC_SLSH,       KC_LCBR, KC_LABK, KC_LBRC, KC_LPRN, KC_G,
-        KC_LSFT,            KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,     _NUM,
+        KC_LSFT,            KC_Z,          KC_X,    KC_C,    KC_V,    KC_B,   KC_F22,
         LT(_SYM,KC_GRV), KC_QUOT, LALT(KC_LSFT), KC_LEFT, KC_RGHT,
 
                                               ALT_T(KC_APP),  KC_LGUI,
@@ -180,7 +180,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                     KC_TRNS,
                                   KC_TRNS, KC_TRNS, KC_TRNS,
     // right hand
-       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+       KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
@@ -202,7 +202,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                               KC_SPC,  KC_LCTL, KC_LGUI,
         // right hand
          KC_RGHT,     KC_6,   KC_7,       KC_8,    KC_9,    KC_0,  KC_MINS,
-         TG(_SYM),    KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,  KC_BSPC,
+         KC_TRNS,    KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,  KC_BSPC,
                       KC_H,   KC_J,       KC_K,    KC_L, KC_SCLN,  KC_QUOT,
          MEH_T(KC_NO),KC_N,   KC_M,    KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
                               KC_RALT, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT,
